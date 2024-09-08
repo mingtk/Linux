85f00e4462098de02c5760cfde2f940e5ba95e86
张梦 <497269343@qq.com>
2024-09-08 16:49:53 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -1,6 +1,6 @@
-DEFAULT_START_PORT=10009                         #默认起始端口
-DEFAULT_SOCKS_USERNAME="meng"                   #默认socks账号
-DEFAULT_SOCKS_PASSWORD="Aa.meng"               #默认socks密码
+DEFAULT_START_PORT=20000                         #默认起始端口
+DEFAULT_SOCKS_USERNAME="userb"                   #默认socks账号
+DEFAULT_SOCKS_PASSWORD="passwordb"               #默认socks密码
 DEFAULT_WS_PATH="/ws"                            #默认ws路径
 DEFAULT_UUID=$(cat /proc/sys/kernel/random/uuid) #默认随机UUID
 
